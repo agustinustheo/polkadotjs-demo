565f9fda655f716e13e699b1f263ccd2aed77cbd
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2021-04-17 14:37:40 +0700

    feat: kalau panggil kevin masalahnya selesai :)

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import { mnemonicGenerate } from '@polkadot/util-crypto'
 import { Keyring } from '@polkadot/keyring'
 import { ApiPromise, WsProvider } from '@polkadot/api'
 import types from './types.json'
+// import { u8aToString } from '@polkadot/util'
 
 export default {
   name: 'App',
@@ -22,36 +23,77 @@ export default {
   async mounted() {
     // let acc = await web3Accounts()
     // console.log(acc);
+    // const { ethereum } = window;
+    // if(ethereum && ethereum.isMetaMask){
+    //   alert('MetaMask installed')
+    //   await ethereum.request({ method: 'eth_requestAccounts' });
+    // }
+    // else{
+    //   alert('MetaMask not installed')
+    // }
     this.generateAccount()
     await this.connectToBlockchain()
-    await this.getBalance(
-      '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY'
-    )
+    await this.getService()
+    // await this.getBalance(this.pair.address)
   },
   methods: {
     generateAccount() {
-      const keyring = new Keyring({ type: 'ed25519', ss58Format: 42 })
-      const mnemonic = mnemonicGenerate()
-      this.mnemonic = mnemonic
-      const pair = keyring.addFromUri(mnemonic, { name: 'first pair' }, 'ed25519');
-      this.pair = pair
-      console.log(this.pair)
+      // type: ed25519, ssFormat: 42 (all defaults)
+      const keyring = new Keyring()
+      this.mnemonic = mnemonicGenerate()
+
+      console.log(this.mnemonic)
+      this.pair = keyring.addFromUri(this.mnemonic, 'ed25519')
+
+      console.log(this.pair.address)
     },
     async connectToBlockchain() {
-      const wsProvider = new WsProvider('wss://debio.theapps.dev/node');
+      const wsProvider = new WsProvider('wss://debio.theapps.dev/node')
       const api = await ApiPromise.create({
         provider: wsProvider,
         types: types
-      });
+      })
       this.api = api
     },
+    async getService(){
+      // // Retrieve the chain & node information information via rpc calls
+      // const [chain, nodeName, nodeVersion] = await Promise.all([
+      //   this.api.rpc.system.chain(),
+      //   this.api.rpc.system.name(),
+      //   this.api.rpc.system.version()
+      // ])
+      // console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`)// Retrieve the chain name
+      
+      // // Retrieve chain properties
+      // const chainInfo = await this.api.registry.getChainProperties()
+      // console.log(`Chain properties ${chainInfo}`)
+
+      // Insert chain properties
+      const data = await this.api.tx.labs.registerLab({
+        "name": "Singapore Hololive Lab Kawaii",
+        "country": "Singapore",
+        "city": "Singapore",
+        "address": "you will never know"
+      })
+      .signAndSend(this.pair)
+      console.log(data)
+
+      
+      // Retrieve chain properties
+      const data2 = await this.api.query.labs.labsByCountryCity("Indonesia", "Jakarta")
+      const keyring = new Keyring()
+      console.log(keyring.encodeAddress(data2.value[0], 42))
+      
+      // // Retrieve the latest header
+      // const lastHeader = await this.api.rpc.chain.getHeader()
+      // console.log(`Latest blockchain header ${lastHeader}`)
+    },
     async getBalance(addr) {
-      const { nonce, data: balance } = await this.api.query.system.account(addr);
-      console.log(nonce)
-      console.log(balance)
-      this.nonce = nonce
-      this.balanceFree = balance.free
-      console.log(`balance of ${balance.free} and a nonce of ${nonce}`);
+      const data = await this.api.query.system.account(addr)
+      console.log(data)
+      // this.nonce = nonce
+      // this.balanceFree = balance.free
+      // console.log(`Nonce and balance free for the account in these addresses ${nonce} and ${balance.free}`)
     }
   }
 }
