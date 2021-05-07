4ef3ee1c96ef3ba29a76ee5ac8ac57289911cda1
Kevin <kevin.janada@gmail.com>
2021-05-07 13:17:47 +0700

    Dummy data fix
    - Handle batch transaction

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <script>
 import { registerLab } from './lib/polkadotProvider/command/labs'
-import { createService } from './lib/polkadotProvider/command/services'
+// import { createService } from './lib/polkadotProvider/command/services'
 import { queryLabsByCountryRegionCity } from './lib/polkadotProvider/query/labs'
 import { queryBalance } from './lib/polkadotProvider/query/balance'
 import { Keyring } from '@polkadot/keyring'
@@ -21,11 +21,15 @@ export default {
     api: null,
   }),
   async mounted() {
-    this.generateAccount()
-    await this.connectToBlockchain()
-    await this.createLab()
-    await this.createService()
-    this.getBalance()
+    try {
+      this.generateAccount()
+      await this.connectToBlockchain()
+      await this.createLab()
+      await this.createService()
+      this.getBalance()
+    } catch (err) {
+      console.log(err)
+    }
   },
   methods:{
     generateAccount() {
@@ -36,7 +40,7 @@ export default {
       console.log(this.mnemonic)
       this.pair = keyring.addFromUri(this.mnemonic, 'ed25519')
 
-      console.log(this.pair.address)
+      console.log('this is the address', this.pair.address)
     },
     async connectToBlockchain() {
       const wsProvider = new WsProvider('wss://debio.theapps.dev/node')
@@ -50,35 +54,62 @@ export default {
       const data = await registerLab(this.api, this.pair, {
         "name": "Singapore Hololive Lab Kawaii",
         "email": "email@mail.com",
-        "country": "SG-SG",
+        "country": "SG",
+        "region": "SG",
         "city": "SG",
         "address": "you will never know"
       });
       console.log(data)
       
       // Retrieve chain properties
-      const labData = await queryLabsByCountryRegionCity(this.api, "SG", "SIN")
+      const labData = await queryLabsByCountryRegionCity(this.api, "SG-SG", "SIN")
       console.log(labData)
     },
     async createService(){
+      let lab = await this.api.query.labs.labs(this.pair.address)
+      lab = lab.toHuman()
+      if (lab.services.length >= 10 ) {
+        console.log('already have at least 10 services')
+        await this.deleteAllServices()
+      }
       // Insert chain properties
       for(let i = 1; i <= 10; i++){
-        const data2 = await createService(this.api, this.pair, {
-          "name": "Singapore Hololive Genetics 2",
-          "price": "2",
-          "category": "genetic",
-          "description": "Singapore Hololive Genetics 2 Description"
-        });
-        console.log(data2)
+        // Need the nonce to make consecutive transactions
+        // retrieve sender's next index/nonce, taking txs in the pool into account
+        const nonce = await this.api.rpc.system.accountNextIndex(this.pair.address);
+
+        const txHash = await this.api.tx.services
+          .createService({
+            "name": "Singapore Hololive Genetics " + i,
+            "price": "2",
+            "category": "genetic",
+            "description": `Singapore Hololive Genetics ${i} Description`
+          })
+          .signAndSend(this.pair, { nonce }) // Pass the nonce here
+
+        console.log('Created Service')
+        console.log(`txHash: ${txHash}`)
       }
-      
       // Retrieve chain properties
+      // FIXME: query ini sudah tidak ada 
       // const serviceData = await queryServicesByCountryRegionCity(this.api, "SG", "SIN")
       // console.log(serviceData)
     },
     async getBalance() {
       const data = await queryBalance(this.api, this.pair.address)
-      console.log('Balance: ' + data)
+      console.log('Balance: ' + data.data.free.toString())
+    },
+    async deleteAllServices() {
+      let lab = await this.api.query.labs.labs(this.pair.address)
+      lab = lab.toHuman()
+      for (let serviceId of lab.services) {
+        const txHash = await this.api.tx.services
+          .deleteService(serviceId)
+          .signAndSend(this.pair, { nonce: -1 }) // use nonce -1 to have the api automatically configure correct nonce
+
+        console.log(`deleted service: ${serviceId}`)
+        console.log(`txHash: ${txHash}`)
+      }
     }
   },
 }
